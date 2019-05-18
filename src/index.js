ff7f15c897a5561e35427863eebac7052780770b
James Madden <jamesbmadden@gmail.com>
2019-05-18 11:26:41 -0700

    Add Image For the Code of Simple-Counter

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ const HOME = new Component('fc-home', ({version}) => {
       h1, h2 {
         text-align: center;
       }
+      img {
+        width: 100%;
+      }
     </style>
-    <h1>Functional Components v${version}</h1>
+    <h1>Functional Web Components v${version}</h1>
     <h2>Custom Elements, Built with Functions & Lit-Html</h2>
     <p>The Power of Lit-Element with the Simplicity of Function-Based Components</p>
     <h3>Example: Simple Counter</h3>
     <simple-counter count="0"></simple-counter>
+    <img src="https://raw.githubusercontent.com/jamesbmadden/fwc/master/img/counter-example-0.2.png">
   `;
 });
 
